dd22e3eb9ba7f5c6c52629604fe9b4b4fbb7f64a
phamngocthinh <phamngocthinh@localhost>
2020-09-20 10:25:41 +0700

    Create packages

--- src/com/wedding/controller/AuthenAccountApiController.java
+++ src/com/wedding/controller/AuthenAccountApiController.java
@@ -1,5 +1,17 @@
 package com.wedding.controller;
 
-public class AuthenAccountApiController {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AuthenAccountApiController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("aaa");
+	}
+	
 }
