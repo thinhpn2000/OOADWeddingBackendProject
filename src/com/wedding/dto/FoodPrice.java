31188e71399c65a9afcac2b556abf74352550fc7
ngocthinhboyy <ngocthinh1c1@gmail.com>
2020-12-26 09:23:24 +0700

    add models foodprice, monthrevenueDTO, servicePrice, add api to get service

---
+++ src/com/wedding/dto/FoodPrice.java
@@ -0,0 +1,57 @@
+package com.wedding.dto;
+
+public class FoodPrice {
+	private int foodID;
+	private int foodPrice;
+	private String foodName;
+	
+	public FoodPrice() {}
+	
+	
+
+
+
+	public FoodPrice(int foodID, int foodPrice, String foodName) {
+		super();
+		this.foodID = foodID;
+		this.foodPrice = foodPrice;
+		this.foodName = foodName;
+	}
+
+
+
+
+
+	public String getFoodName() {
+		return foodName;
+	}
+
+
+
+
+	public void setFoodName(String foodName) {
+		this.foodName = foodName;
+	}
+
+
+
+
+
+	public int getFoodID() {
+		return foodID;
+	}
+
+	public void setFoodID(int foodID) {
+		this.foodID = foodID;
+	}
+
+	public int getFoodPrice() {
+		return foodPrice;
+	}
+
+	public void setFoodPrice(int foodPrice) {
+		this.foodPrice = foodPrice;
+	};
+	
+	
+}
